c120c861ff9f414d1dc53348b7872570cf8b6562
romenigld <romenigld@gmail.com>
2017-01-18 20:00:09 +0100

    Add title functionality for project show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # the splat operator (*), any arguments passed from this point will be available in the method as an array. Here that array can be referenced as parts.
+  def title(*parts)
+    unless parts.empty?
+      content_for :title do
+        (parts << "Ticketee").join(" - ")
+      end
+    end
+  end
 end
--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -11,5 +11,12 @@ RSpec.feature "Users can create new projects" do
     click_button "Create Project"
 
     expect(page).to have_content "Project has been created."
+
+    project = Project.find_by(name: "Sublime Text 3")
+    # ensures that you’re on what should be the show action in the ProjectsController.
+    expect(page.current_url).to eq project_url(project)
+
+    title = "Sublime Text 3 - Projects - Ticketee"
+    expect(page).to have_title title
   end
 end
