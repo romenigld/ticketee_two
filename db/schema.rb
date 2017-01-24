b0c54689124c65f46f0ea9e0736e9de08578dc29
romenigld <romenigld@gmail.com>
2017-01-24 20:17:38 +0100

    Add feature for archiving users, including protection against self-archiving

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170123234747) do
+ActiveRecord::Schema.define(version: 20170124190158) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20170123234747) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin",                  default: false
+    t.datetime "archived_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
