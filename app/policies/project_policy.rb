87e5246020f1c88686e8439ab4473ae0888eb2ef
romenigld <romenigld@gmail.com>
2017-01-25 19:37:47 +0100

    Don't list projects that a user doesn't have permission to see

--- app/policies/project_policy.rb
+++ app/policies/project_policy.rb
@@ -1,7 +1,11 @@
 class ProjectPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope
+  # none is a convenience method provided by Active Record to automatically return no records
+      return scope.none if user.nil?
+      return scope.all if user.admin?
+
+      scope.joins(:roles).where(roles: { user_id: user })
     end
   end
 
