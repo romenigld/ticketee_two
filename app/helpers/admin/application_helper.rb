104c41f01d8345745480277da952318607a5cc16
romenigld <romenigld@gmail.com>
2017-01-30 18:51:52 +0100

    Admins can edit project roles of existing users

--- app/helpers/admin/application_helper.rb
+++ app/helpers/admin/application_helper.rb
@@ -1,2 +1,20 @@
 module Admin::ApplicationHelper
+  def roles
+    hash = {}
+
+    Role.available_roles.each do |role|
+      hash[role.titleize] = role
+    end
+
+    hash
+  end
+
+# This was substituted for the other example because if adds a new roles? then the other version is better
+  # def roles
+  #   {
+  #     'Manager' => 'manager',
+  #     'Editor'  => 'editor',
+  #     'Viewer'  => 'viewer'
+  #   }
+  # end
 end
