9da60a01ab83871679ac36c123a28b7d26b2a440
romenigld <romenigld@gmail.com>
2017-02-07 14:26:41 +0100

    Only admins and managers can change states of a ticket

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,8 @@ end
 end
 
 unless State.exists?
-  State.create(name: "new", color: "#0066CC")
+  State.create(name: "New", color: "#0066CC", default: true)
   State.create(name: "Open", color: "#008000")
   State.create(name: "Closed", color: "#990000")
-  State.create(name: "Awesome", color: "#663399")  
+  State.create(name: "Awesome", color: "#663399")
 end
