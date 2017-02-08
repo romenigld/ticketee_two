e58c65dc001339ebf8d2fdb5b5ee9b2462a8865d
romenigld <romenigld@gmail.com>
2017-02-08 20:15:44 +0100

    Users can add tags when adding a comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,8 @@ class Comment < ActiveRecord::Base
   belongs_to :ticket
   belongs_to :author, class_name: "User"
 
+  attr_accessor :tag_names
+
   scope :persisted, lambda { where.not(id: nil) }
 
   validates :text, presence: true
@@ -12,6 +14,8 @@ class Comment < ActiveRecord::Base
 
   before_create :set_previous_state
   after_create :set_ticket_state
+  # You want to use an after_create here so that the tags aren’t associated prematurely with a ticket, which they would be if you used a before_create. When you add things to an association with methods like ticket.tags << tag, you don’t have to save anything for that association to be created; it happens on the spot. So if the comment isn’t valid and doesn’t get saved, the tags would still be added—oops!
+  after_create :associate_tags_with_ticket
 
   private
 
@@ -22,5 +26,13 @@ class Comment < ActiveRecord::Base
   def set_ticket_state
     ticket.state = state
     ticket.save!
+  end
+
+  def associate_tags_with_ticket
+    if tag_names
+      tag_names.split.each do |name|
+        ticket.tags << Tag.find_or_create_by(name: name)
+      end
+    end
   end
 end
