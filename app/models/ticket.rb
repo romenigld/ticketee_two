11d4b117bf7920281e9d569b9ff074241fb86689
romenigld <romenigld@gmail.com>
2017-02-08 19:26:47 +0100

    Users can tag tickets upon creation

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,15 +3,25 @@ class Ticket < ActiveRecord::Base
   belongs_to :author, class_name: "User"
   belongs_to :state
   has_many :comments, dependent: :destroy
+  has_and_belongs_to_many :tags, uniq: true
   has_many :attachments, dependent: :destroy
   accepts_nested_attributes_for :attachments, reject_if: :all_blank
+
+  attr_accessor :tag_names
 
   validates :name, presence: true
   validates :description, presence: true,
                           length: { minimum: 10 }
 
-   before_create :assign_default_state
+  before_create :assign_default_state
 
+  def tag_names=(names)
+    @tag_names = names
+    names.split.each do |name|
+      self.tags << Tag.find_or_initialize_by(name: name)
+    end
+  end
+  
    private
 
    def assign_default_state
