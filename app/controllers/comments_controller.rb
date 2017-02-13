614fa74a4796b28990b53ce743d0d0d610851d96
romenigld <romenigld@gmail.com>
2017-02-13 17:06:27 +0100

    Restrict tagging to only authorized users

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,8 @@ class CommentsController < ApplicationController
   before_action :set_ticket
 
   def create
-    whitelisted_params = comment_params
 
-    unless policy(@ticket).change_state?
-      whitelisted_params.delete(:state_id)
-    end
-
-    @comment = @ticket.comments.build(whitelisted_params)
+    @comment = @ticket.comments.build(sanitized_parameters)
     @comment.author = current_user
     authorize @comment, :create?
 
@@ -24,11 +19,25 @@ class CommentsController < ApplicationController
 
   private
 
-  def set_ticket
-    @ticket = Ticket.find(params[:ticket_id])
-  end
+    def set_ticket
+      @ticket = Ticket.find(params[:ticket_id])
+    end
 
-  def comment_params
-    params.require(:comment).permit(:text, :state_id, :tag_names)
-  end
+    def comment_params
+      params.require(:comment).permit(:text, :state_id, :tag_names)
+    end
+
+    def sanitized_parameters
+      whitelisted_params = comment_params
+
+      unless policy(@ticket).change_state?
+        whitelisted_params.delete(:state_id)
+      end
+
+      unless policy(@ticket).tag?
+        whitelisted_params.delete(:tag_names)
+      end
+
+      whitelisted_params
+    end
 end
