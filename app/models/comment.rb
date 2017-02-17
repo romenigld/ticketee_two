20aaead2bf59c48b1aa65281a958b23d6b11a94a
romenigld <romenigld@gmail.com>
2017-02-17 15:55:34 +0100

    Automatically subscribe users to a ticket when they comment on it

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -16,6 +16,7 @@ class Comment < ActiveRecord::Base
   after_create :set_ticket_state
   # You want to use an after_create here so that the tags aren’t associated prematurely with a ticket, which they would be if you used a before_create. When you add things to an association with methods like ticket.tags << tag, you don’t have to save anything for that association to be created; it happens on the spot. So if the comment isn’t valid and doesn’t get saved, the tags would still be added—oops!
   after_create :associate_tags_with_ticket
+  after_create :author_watches_ticket
 
   private
 
@@ -33,6 +34,12 @@ class Comment < ActiveRecord::Base
       tag_names.split.each do |name|
         ticket.tags << Tag.find_or_create_by(name: name)
       end
+    end
+  end
+
+  def author_watches_ticket
+    if author.present? && !ticket.watchers.include?(author)
+      ticket.watchers << author
     end
   end
 end
